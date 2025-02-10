fd3dd73524d512799e8bc58744b8e79b92b283ad
Syarwin <timothee.pecatta@ens-lyon.fr>
2025-02-10 09:35:03 +0100

    Material datas for scenario 2

--- misc/editor/script.js
+++ misc/editor/script.js
@@ -4,8 +4,8 @@ let DATAS = null;
 let modes = localStorage.getItem('wttmModes');
 modes = modes === null ? {} : JSON.parse(modes);
 
-const EXCLUSIVE_MODES = ['add'];
-const ONCLICK_MODES = ['add'];
+const EXCLUSIVE_MODES = ['add', 'move'];
+const ONCLICK_MODES = ['add', 'rotate'];
 
 let editor = CodeMirror.fromTextArea($('data'), {
   lineNumbers: true,
@@ -136,7 +136,7 @@ $('form-load-storage').addEventListener('submit', async (evt) => {
   for (let i = 0; i < fileList.length; i++) {
     let name = fileList[i].name;
     let ext = name.split('.').pop();
-    if (ext == 'wttm') wttmFile = fileList[i];
+    if (ext == 'json') wttmFile = fileList[i];
   }
   if (wttmFile == null) {
     alert('Please either select a file on browser storage or select a wttm file on your computer');
@@ -158,7 +158,7 @@ function updateLoadFile() {
     names.push(fileList[i].name);
   }
 
-  $('load-file-label').innerHTML = names.length ? names.join(', ') : 'Click here to load .wttm file';
+  $('load-file-label').innerHTML = names.length ? names.join(', ') : 'Click here to load .json file';
 }
 updateLoadFile();
 
@@ -173,15 +173,6 @@ updateLoadFile();
 function loadScenario(datas) {
   DATAS = datas;
   if (!DATAS.sections) DATAS.sections = [];
-  // DATAS.sections = [
-  //   {
-  //     id: 'numbers',
-  //     name: 'Numbers',
-  //     modes: ['show', 'add'],
-  //     elts: [],
-  //     eltClass: 'slot-number',
-  //   },
-  // ];
 
   $('display-scenario-id').innerHTML = 'ID: ' + DATAS.id;
   $('display-scenario-name').innerHTML = 'Name: ' + DATAS.name;
@@ -200,31 +191,15 @@ function loadScenario(datas) {
     document.location.search = params;
   }
 
+  // Load css
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = `./${DATAS.id}.css`;
+  document.head.appendChild(link);
+
   saveScenario();
 }
-
-// //////////////////
-// //////////////////
-// ///  CHANGE JPG
-// //////////////////
-// //////////////////
-// $('scenario-change-jpg').addEventListener('click', async () => {
-//   let jpgFile = null;
-//   while (jpgFile == null) {
-//     let url = window.prompt('URL of the jpg file');
-//     if (url == null) return;
-//     let validURL = await testURL(url);
-//     if (validURL) {
-//       jpgFile = url;
-//     } else {
-//       alert('Invalid jpg file');
-//     }
-//   }
-
-//   DATAS.jpgUrl = jpgFile;
-//   $('board').style.backgroundImage = `url(${jpgFile})`;
-//   saveScenario();
-// });
 
 // //////////////////
 // //////////////////
@@ -240,7 +215,6 @@ function saveScenario() {
   localStorage.setItem('wttmScenarios', JSON.stringify(scenarios));
   editor.getDoc().setValue(JSON.stringify(DATAS, null, 2));
   $('save-changes').classList.add('disabled');
-  updateStatus();
 }
 
 ///////////////////////////////////////////////
@@ -254,10 +228,6 @@ function saveScenario() {
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
-
-function updateStatus() {
-  return;
-}
 
 // Toggle a mode (update button, storage, and update class)
 function toggleMode(section, mode, val = null) {
@@ -273,6 +243,7 @@ function toggleMode(section, mode, val = null) {
       config.modes.forEach((mode2) => {
         if (section2 == section && mode2 == mode) return;
         if (!EXCLUSIVE_MODES.includes(mode2)) return;
+        if (!modes[section2]) return;
 
         modes[section2][mode2] = false;
         $('main-frame').classList.remove(`${mode2}-${section2}`);
@@ -303,6 +274,8 @@ function setupSection(config) {
   let icons = {
     show: 'eye',
     add: 'plus',
+    rotate: 'compute',
+    move: 'move',
   };
 
   if (!config.modes) config.modes = [];
@@ -319,19 +292,31 @@ function setupSection(config) {
 
     // ADD MODE
     if (mode == 'add' && config.elts) {
-      config.elts.forEach((elt) => addSlot(elt.id, config.eltClass, elt.x, elt.y));
+      config.elts.forEach((elt) => addSlot(elt.id, config.eltClass, elt.x, elt.y, elt.r || 0));
     }
   });
 }
 
-function addSlot(id, className, x, y) {
+function addSlot(id, className, x, y, r = 0) {
   $('board').insertAdjacentHTML('beforeend', `<div class='wttm-slot ${className}' id='slot-${id}' data-id='${id}'></div>`);
   $(`slot-${id}`).style.left = x + 'px';
   $(`slot-${id}`).style.top = y + 'px';
+  if (r != 0) {
+    $(`slot-${id}`).style.transform = `rotate(${r}deg)`;
+  }
+}
+
+function moveSlot(slot, dx, dy) {
+  let oSlot = $(`slot-${dragged.id}`);
+  slot.x += dx;
+  slot.y += dy;
+  oSlot.style.left = `${slot.x}px`;
+  oSlot.style.top = `${slot.y}px`;
+  saveScenario();
 }
 
 // Click on Board
-function onClickBoard(sectionConfig, mode, pos) {
+function onClickBoard(sectionConfig, mode, pos, evt) {
   // ADD MODE
   if (mode == 'add') {
     let maxId = 0;
@@ -342,6 +327,21 @@ function onClickBoard(sectionConfig, mode, pos) {
 
     addSlot(id, sectionConfig.eltClass, pos.x, pos.y);
     sectionConfig.elts.push({ id, x: pos.x, y: pos.y });
+    saveScenario();
+  }
+
+  // ROTATE MODE
+  if (mode == 'rotate') {
+    const DELTA = 5;
+    let slotId = evt.target.classList.contains('wttm-slot') ? evt.target.dataset.id : null;
+    if (slotId == null) return;
+
+    sectionConfig.elts.forEach((elt) => {
+      if (elt.id != parseInt(slotId)) return;
+
+      elt.r = (elt.r || 0) + (evt.ctrlKey ? -DELTA : DELTA);
+      evt.target.style.transform = `rotate(${elt.r}deg)`;
+    });
     saveScenario();
   }
 }
@@ -369,11 +369,36 @@ $('main-frame').addEventListener('click', (evt) => {
       if (!modes[section][mode]) return;
 
       // Callback
-      onClickBoard(config, mode, { x, y });
+      onClickBoard(config, mode, { x, y }, evt);
     });
   });
 });
 
+// Dragndrop elements
+let dragged = null;
+$('main-frame').addEventListener('mousedown', (evt) => {
+  // Are we listening to MOVE mode
+  DATAS.sections.forEach((config) => {
+    let section = config.id;
+    if (!config.modes.includes('move') || !modes[section].move) return;
+    if (!evt.target.classList.contains(config.eltClass)) return;
+
+    config.elts.forEach((elt) => {
+      if (elt.id == evt.target.dataset.id) {
+        dragged = elt;
+      }
+    });
+  });
+});
+
+$('main-frame').addEventListener('mouseup', (evt) => {
+  if (dragged) {
+    moveSlot(dragged, evt.movementX, evt.movementY);
+  }
+  dragged = null;
+});
+
+// Move over slots
 $('main-frame').addEventListener('mousemove', (evt) => {
   let wrapper = $('main-frame-wrapper');
   let x = evt.x - wrapper.offsetLeft + wrapper.scrollLeft;
@@ -382,6 +407,11 @@ $('main-frame').addEventListener('mousemove', (evt) => {
   $('hover-indicator').style.top = y + 'px';
 
   $('cell-indicator-counter').innerHTML = evt.target.classList.contains('wttm-slot') ? evt.target.id : '----';
+
+  // Dragndrop
+  if (dragged) {
+    moveSlot(dragged, evt.movementX, evt.movementY);
+  }
 });
 
 // Add new section
@@ -391,9 +421,9 @@ $('add-section').addEventListener('click', () => {
 
   DATAS.sections.push({
     id: id,
-    name: id,
-    modes: ['show', 'add'],
-    eltClass: '',
+    name: capitalizeFirstLetter(id),
+    modes: ['show', 'add', 'move'],
+    eltClass: `slot-${id.slice(0, -1)}`,
     elts: [],
   });
   saveScenario();
@@ -456,4 +486,8 @@ async function parseJsonFile(file) {
     fileReader.onerror = (error) => reject(error);
     fileReader.readAsText(file);
   });
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
