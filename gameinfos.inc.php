90e503a2eac26109e6d8a1e28000b317cfd725a5
Syarwin <timothee.pecatta@ens-lyon.fr>
2025-03-08 23:10:01 +0100

    159500: "on phone! when the game opens in the beginning it does not fit the screen"

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -93,5 +93,6 @@ $gameinfos = [
         //  maximum possible value: 740 (ie: your game interface should fit with a 740px width (correspond to a 1024px screen)
         //  minimum possible value: 320 (the lowest value you specify, the better the display is on mobile)
         'min' => 740,
+        'autoscale' => 'viewport',
     ],
 ];
